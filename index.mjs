3d3afb59badd7fc886179b6a3054f350a9a6c125
Jon Ander Beroz Basterretxea <jonanberoz2000@gmail.com>
2023-07-23 00:21:25 +0200

    repo init

---
+++ index.mjs
@@ -0,0 +1,9 @@
+import { multiply, sum, mod } from "mathy";
+
+console.log("hello world!");
+
+let a = sum(5, 4);
+console.log(a);
+
+console.log(multiply(5, 4));
+console.log(mod(8, 4));
---
+++ node_modules/mathy/index.mjs
@@ -0,0 +1,23 @@
+export function sum(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+export function multiply(a, b) {
+  return a * b;
+}
+
+export function divide(a, b) {
+  return a / b;
+}
+
+export function mod(a, b) {
+  return a % b;
+}
+
+export function max(a, b) {
+  return a;
+}
